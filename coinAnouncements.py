70a7df58efd2edd698853dfbc996ce5fec1de525
Alex Pimania <randomiseme33@gmail.com>
2017-04-16 08:57:53 +1000

    Update coinAnouncements.py

--- coinAnouncements.py
+++ coinAnouncements.py
@@ -1,68 +1,61 @@
 def getSearchTerms():
    import json
-   coinNames = json.loads(open("coinNames.txt").read())
-   eventNames = json.loads(open("eventNames.txt").read())
-   return [coinNames, eventNames]
+   return json.loads(open("searchTerms.txt").read())
+
+def removeLink(text):
+   import re
+   link = re.search("(?P<url>https?://[^\s]+)", text)
+   if link:
+      link = link.group("url")
+      text = text.replace(link, "").replace("  ", " ")
+   return text
 
 def extractTweets(events):
    import pickle
-   import datefinder
+   from datefinder import find_dates as extractDate
    import time
    
    tweets = pickle.loads(open("savedTweets.txt", "rb").read())
-   validTweets = []
+   announcements = []
    for tweet in tweets:
-      tweetJson = tweet._json
-      userId = tweetJson["id"]
-      tweetText = tweetJson["full_text"]
-      retweets = tweetJson["retweet_count"]
-      usersPosted = []
+      data = tweet._json
+      text = removeLink(data["full_text"])
+      likes = data["favorite_count"]
+      dateText = data["created_at"]
       
-      tweetDateText = tweet._json["created_at"]
-      tweetDateTuple = [date for date in datefinder.find_dates(tweetDateText)][0].timetuple()
-      tweetDate = time.mktime(tweetDateTuple)
-      currentTime = time.time()
-      timeSinceTweet = (currentTime - tweetDate) * 0.01
+      dateTuple = [date for date in extractDate(dateText)][0].timetuple()
+      ageDays = (time.time() - time.mktime(dateTuple)) / (24*60*60)
       
-      importance = retweets/timeSinceTweet
-      if any(event in tweetText for event in events) and not userId in usersPosted and not tweetText in [tweet["tweet"] for tweet in validTweets]: 
-         validTweets.append({"tweet":tweetText, "date":"", "importance":importance})
-         usersPosted.append(userId) 
-           
-   return validTweets
+      importance = likes
+      if any(event in text.lower() for event in events) and not text.lower() in [tweet["tweet"].lower() for tweet in announcements]: 
+         announcements.append({"tweet" : text, "importance" : importance, "ageDays" : ageDays})
+
+   return announcements
    
 
-def categorizeTweets(tweets):
+def getCoinTweets(tweets):
    coinTweets = {}
    for tweet in tweets:
       for coin in coinNames:
          if coin in tweet["tweet"]:
             if not coin in coinTweets.keys():
                coinTweets[coin] = {}
-               coinTweets[coin]["tweets"] = []
-               coinTweets[coin]["dates"] = []
+               coinTweets[coin]["tweets"] = {}
                coinTweets[coin]["importance"] = 0
                
-            coinTweets[coin]["tweets"].append(tweet["tweet"])
+            coinTweets[coin]["tweets"][tweet["tweet"]] = tweet["ageDays"]
             coinTweets[coin]["importance"] += tweet["importance"]
    return coinTweets
-   
 
-def extractEventDates(coinTweets):
-   import datefinder
-   coinEvents = {}
-   for coin in coinTweets:
-      coinDates = []
-      for tweet in coinTweets[coin]["tweets"]:
-         coinDates.extend([str(date) for date in datefinder.find_dates(tweet)])
-      coinTweets[coin]["dates"] = coinDates
-   return coinTweets
          
          
 coinNames, eventNames = getSearchTerms() 
 validTweets = extractTweets(eventNames)
-categorizedTweets = categorizeTweets(validTweets)
-coinEvents = extractEventDates(categorizedTweets)
-for coin in coinEvents:
-   print("Coin: " + coin + "\nTweets: \n" + "\n".join(coinEvents[coin]["tweets"]) + "\nDates: \n" + "\n".join(coinEvents[coin]["dates"]) + "\nImportance: " + str(coinEvents[coin]["importance"]))
+coinTweets = getCoinTweets(validTweets)
+for coin in sorted(coinTweets, key=lambda k: 0 - coinTweets[k]["importance"]):
+   tweets = coinTweets[coin]["tweets"]
+   print("Coin: " + coin)
+   print("Importance: " + str(coinTweets[coin]["importance"]))
+   for tweet in tweets:
+      print(tweet + " || Age: " + str(round(tweets[tweet], 2)) + " days")
    print("\n\n")
