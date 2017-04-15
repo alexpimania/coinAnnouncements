8ca303d7d52c8bfc1b4b29236edbbe83e3bfd737
Alex Pimania <randomiseme33@gmail.com>
2017-04-15 11:52:06 +1000

    Add files via upload

---
+++ coinAnouncements.py
@@ -0,0 +1,62 @@
+def extractTweets():
+   import json
+   import datefinder
+   import time
+   tweets = json.loads(open("savedTweets.txt").read())
+   
+   for tweet in tweets:
+      tweetJson = tweet._json
+      userId = tweetJson["id"]
+      tweetText = tweetJson["text"]
+      retweets = tweetJson["retweet_count"]
+      
+      tweetDateText = tweet._json["created_at"]
+      tweetDateTuple = [date for date in datefinder.find_dates(tweetDate)][0].timetuple()
+      tweetDate = time.mktime(dateTimeTweetDate)
+      currentTime = time.time()
+      timeSinceTweet = (currentTime - currentTime) * 0.01
+      
+      importance = retweets/timeSinceTweet
+      
+      for event in events:
+         if event in tweetText and not userId in usersPosted:
+            validTweets.append({"tweet":tweetText, "date":"", "importance":importance})
+            usersPosted.append(userId)
+               
+   return validTweets
+   
+
+def categorizeTweets(tweets):
+   coinTweets = {}
+   for tweet in tweets:
+      for coin in coinNames:
+         if coin in tweet["tweet"]:
+            if not coin in coinTweets.keys():
+               coinTweets[coin] = {}
+               coinTweets[coin]["tweets"] = []
+               coinTweets[coin]["dates"] = []
+               coinTweets[coin]["importance"] = 0
+               
+            coinTweets[coin]["tweets"].append(tweet["tweet"])
+            coinTweets[coin]["importance"] += tweet["importance"]
+   return coinTweets
+   
+
+def extractEventDates(coinTweets):
+   import datefinder
+   coinEvents = {}
+   for coin in coinTweets:
+      coinDates = []
+      for tweet in coinTweets[coin]["tweets"]:
+         for date in datefinder.find_dates(tweet):
+            coinDates.append(date)
+      coinEvent[coin] = {"tweets": coinTweets[coin]["tweets"], "dates":coinDates, "importance":coinTweets[coin]["importance"]}
+      return coinEvents
+         
+
+validTweets = getCoinTweets()
+categorizedTweets = categorizeTweets(validTweets)
+coinEvents = extractEventDates(categorizedTweets)
+for coin in coinEvents:
+   print("Coin: \n\n" + coin["coin"] + "\n\n\nTweets: \n\n" + "\n".join(coin["tweets"]) + "\n\n\nDates: \n\n" + "\n".join(coin["dates"]))
+   print("\n\n\n\n")
---
+++ downloadData.py
@@ -0,0 +1,47 @@
+daysToScan = 3
+
+def getSearchTerms():
+   import json
+   coinNames = json.loads(open("coinNames.txt").read())
+   eventNames = json.loads(open("eventNames.txt").read())
+   return [coinNames, eventNames]
+
+
+def chunks(listToCut, maxLength):
+    for i in range(0, len(listToCut), maxLength):
+        yield listToCut[i:i+maxLength]
+        
+        
+def initTwitterApi():
+   import tweepy
+   twitterKeys = open("twitterKeys.txt").read().strip().split() 
+   auth = tweepy.OAuthHandler(twitterKeys[0], twitterKeys[1])
+   auth.set_access_token(twitterKeys[2], twitterKeys[3])
+   api = tweepy.API(auth, wait_on_rate_limit_notify=True, wait_on_rate_limit=True)
+   return api
+    
+def downloadTweets(daysToScan):
+   import tweepy
+   import time
+   from datetime import datetime
+   
+   tweets = []
+   sinceDate=datetime.fromtimestamp(time.time() - 60*60*24 * daysToScan).strftime('%Y-%m-%d')
+   for chunk in chunks(coinNames, 10):
+      coinsQuery = " OR ".join(chunk)
+      for tweet in tweepy.Cursor(api.search, q=coinsQuery, since=sinceDate, lang="en").items(1000):
+         tweets.append(tweet)
+   return tweets
+   
+def saveTweets(tweets):
+   import json
+   with open("savedTweets.txt", "w+") as tweetsFile:
+      tweetsFile.write(json.dumps(tweets))
+      
+
+   
+               
+coinNames, eventNames = getSearchTerms()           
+api = initTwitterApi()
+tweets = downloadTweets(daysToScan)
+saveTweets(tweets)
